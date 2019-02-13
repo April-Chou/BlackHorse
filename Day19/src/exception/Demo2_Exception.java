d047787f152fd53e34c714988a08f5c8fa750fea
April-Chou <zhouchunzhe@gmail.com>
2019-02-13 13:37:59 +0000

    1111

---
+++ Day19/src/exception/Demo2_Exception.java
@@ -0,0 +1,15 @@
+package exception;
+/*
+ * try 用来检测异常
+ * catch 捕获异常
+ * finally 释放资源
+ */
+
+public class Demo2_Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
