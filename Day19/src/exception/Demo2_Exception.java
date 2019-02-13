4406cb0abfcf9845c14d233f24e467b7ecc1a472
April-Chou <zhouchunzhe@gmail.com>
2019-02-13 14:09:32 +0000

    11111

--- Day19/src/exception/Demo2_Exception.java
+++ Day19/src/exception/Demo2_Exception.java
@@ -9,7 +9,22 @@ public class Demo2_Exception {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Demo d1 = new Demo();
+		try {
+			int x = d1.div(10, 2);
+			System.out.println(x);
+		}
+		catch(ArithmeticException a) {
+			System.out.println("chu shu wei 0");
+		}
+			
 
 	}
 
+}
+
+class Demo{
+	public int div(int x, int y) {
+		return x / y;
+	}
 }
