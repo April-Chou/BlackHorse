b4d87d5f9f906b8e87cbf0794a29ba4d248ff134
April-Chou <zhouchunzhe@gmail.com>
2019-02-14 11:26:12 +0000

    22345

---
+++ Day19/src/test/Test1.java
@@ -0,0 +1,16 @@
+package test;
+/*
+ * final 可以修饰类，不能被继承
+ *       方法， 不能被重写	
+ *       变量， 只能赋值一次 
+ * finally try语句中的一个语句体，不能单独使用，用来释放资源
+ * finalize 是一个方法。 当垃圾回收器确定不存在对该对象的引用时，由对象的垃圾回收期调用此方法
+ * 
+ * ru 1如果catch里面有return语句，请问finally语句还会执行吗，请问如果会执行的话，是在return前还是return后。 return后，返回路径里面值没变
+ *  finally里面不要写 return， 这样try和catch里面的语句都没有意义
+ * 
+ */
+
+public class Test1 {
+
+}
