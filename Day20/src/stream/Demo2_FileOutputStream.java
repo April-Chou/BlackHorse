15e91bede283f0f2f83c1cef8251e5d3e7a8151a
April-Chou <zhouchunzhe@gmail.com>
2019-02-20 12:57:52 +0000

    20th/Feb/2019 FileOutPutStream finish

--- Day20/src/stream/Demo2_FileOutputStream.java
+++ Day20/src/stream/Demo2_FileOutputStream.java
@@ -3,18 +3,36 @@ package stream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+/*
+ * Fileoutputstream 在创建对象的时候如果没有这个文件会帮我们创建出来
+ * ru 如果有这个文件就会先将文件清空。
+ */
 
 public class Demo2_FileOutputStream {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		FileOutputStream fos = new FileOutputStream("yyy.txt");//shu chu输出流创建字节输出流对象，如果没有就自动创建一个
+//		demo1();
+		FileOutputStream fos = new FileOutputStream("yyy.txt", true);
 		fos.write(97);
 		fos.write(98);
 		fos.write(99);
 		
-		fos.close();  //sui 虽然写出的是一个int数，但是到文件上的是一个字节，会自动去除前三个八位
 
+	}
+
+	/**
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	@SuppressWarnings("unused")
+	private static void demo1() throws FileNotFoundException, IOException {
+		FileOutputStream fos = new FileOutputStream("yyy.txt");//shu chu输出流创建字节输出流对象，如果没有就自动创建一个
+//		fos.write(97);
+//		fos.write(98);
+//		fos.write(99);
+		fos.write(100);
+		fos.close();  //sui 虽然写出的是一个int数，但是到文件上的是一个字节，会自动去除前三个八位
 	}
 
 }
