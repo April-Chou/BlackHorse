073d869ce0acc209864e9d12d3e74f2121ea7f47
April-Chou <zhouchunzhe@gmail.com>
2019-02-21 12:51:07 +0000

    21th/Feb/2019	lunch

--- Day20/src/stream/Demo5_BufferCopy.java
+++ Day20/src/stream/Demo5_BufferCopy.java
@@ -9,10 +9,22 @@ import java.io.IOException;
 /*
  * nei 内存的运算效率要比硬盘高， 所以只要降低硬盘的读写次数就会提高效率
  * kai 开发中可以小数组也可以buffered
+ * close 方法具备刷新功能，在关闭流之前会刷新一次缓冲区，将缓冲区的字节刷新到文件上，再关闭
+ * flush 也可以刷新缓冲区，刷完之后还可以继续写，close方法刷完之后不能写了
+ * 
  */
 public class Demo5_BufferCopy {
 
 	public static void main(String[] args) throws IOException {
+		demo1();
+
+	}
+
+	/**
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	private static void demo1() throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
 		FileInputStream fis = new FileInputStream("D:\\Github_Repository\\Day20\\test1234.docx");
 		FileOutputStream fos = new FileOutputStream("copy.docx");
@@ -25,7 +37,6 @@ public class Demo5_BufferCopy {
 		}
 		bis.close();
 		bos.close();
-
 	}
 
 }
