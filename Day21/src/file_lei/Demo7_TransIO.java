fa41a74d01bad468fc7bf1c4c56feb0c48af8716
April-Chou <zhouchunzhe@gmail.com>
2019-02-27 15:55:18 +0000

    gbk-utf-8

--- Day21/src/file_lei/Demo7_TransIO.java
+++ Day21/src/file_lei/Demo7_TransIO.java
@@ -1,13 +1,39 @@
 package file_lei;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Demo7_TransIO {
 
 	public static void main(String[] args) throws IOException {
+		//demo1();
+		
+		InputStreamReader irs = new InputStreamReader(new FileInputStream("utf-8.txt"),"utf-8");
+		OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("gbk.txt"),"gbk");
+		
+		int c;
+		while((c = irs.read()) != -1) {
+			osw.write(c);
+		}
+		
+		irs.close();
+		osw.close();
+		
+
+	}
+
+	/**
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	@SuppressWarnings("unused")
+	private static void demo1() throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
 		FileReader fr = new FileReader("utf-8.txt");
 		FileWriter fw = new FileWriter("gbk.txt");
@@ -19,7 +45,6 @@ public class Demo7_TransIO {
 		
 		fr.close();
 		fw.close();
-
 	}
 
 }
