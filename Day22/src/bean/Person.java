26d38cd0a9754b3980092747c9fcf9c5899829c2
April-Chou <zhouchunzhe@gmail.com>
2019-03-05 16:17:15 +0000

    16.17

--- Day22/src/bean/Person.java
+++ Day22/src/bean/Person.java
@@ -3,6 +3,10 @@ package bean;
 import java.io.Serializable;
 
 public class Person implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String name;
 	private int age;
 	/**
