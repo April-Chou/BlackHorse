8bfecfb6e41ecb00d30550f4f3e05632ac62f1a1
April-Chou <zhouchunzhe@gmail.com>
2019-03-07 10:31:29 +0000

    finish day 22

--- Day22/src/otherIO/Demo10_Properties.java
+++ Day22/src/otherIO/Demo10_Properties.java
@@ -1,13 +1,42 @@
 package otherIO;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Properties;
 
 public class Demo10_Properties {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, IOException {
 		// TODO Auto-generated method stub
 //		demo();
+//		demo1();
+		Properties prop = new Properties();
+		System.out.println("Before : " + prop);
+		prop.load(new FileInputStream("config.properties"));
+		System.out.println("After : " + prop);
+		
+//		load and strore
+		/*
+		 * zhi 只set 不strore只是在内存中修改，不是在硬盘中修改，文件的内容是不变的，必须是store里output stream才可以
+		 */
+		prop.setProperty("age", "22");
+		prop.setProperty("Job", "Witcher");
+		/*
+		 * stroe,第二个参数是用来描述文件列表的，如果不描述可以传null
+		 */
+		prop.store(new FileOutputStream("config.properties"), "xxx");
+		System.out.println("Finally : " + prop);
+
+	}
+
+	/**
+	 * 
+	 */
+	@SuppressWarnings("unused")
+	private static void demo1() {
 		Properties prop = new Properties();
 		
 		prop.setProperty("name", "xiaoguo");
@@ -16,6 +45,7 @@ public class Demo10_Properties {
 		System.out.println(prop);
 		
 //		meiju
+		@SuppressWarnings("unchecked")
 		Enumeration<String> en = (Enumeration<String>) prop.propertyNames();
 		
 //		loop
@@ -24,7 +54,6 @@ public class Demo10_Properties {
 			String value = prop.getProperty(key);
 			System.out.println(key + " = " + value);
 		}
-
 	}
 
 	/**
