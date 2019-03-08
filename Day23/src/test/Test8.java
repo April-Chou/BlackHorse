5edea6648546fdfaced88ab82177c779f7ac30c4
April-Chou <zhouchunzhe@gmail.com>
2019-03-08 15:54:45 +0000

    joshph problem not finished

---
+++ Day23/src/test/Test8.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.util.Scanner;
+/*
+ * silu 
+ * 1. chuang 创建输入对象，输入要查询的数
+ * 2. yong 用ArrayList实现集合
+ * 3. 当指针指到 3或者3的倍数的时候，删除这个数，
+ * 4. 当指针指到最大size的时候，归零
+ * 5.反复循环直到最后数字
+ */
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int getLucky() {
+//		step 1
+		System.out.println("Please enter the number which you want to check for Josphen Problem: ");
+		
+//		use if else to check:
+		while(true) {
+			Scanner sc = new Scanner(System.in);
+			String line = sc.nextLine();
+			Integer number = Integer.parseInt(line);
+//			if(())  not finished
+			
+		}
+		
+	}
+
+}
